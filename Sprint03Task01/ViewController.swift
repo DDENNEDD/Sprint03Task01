5dd3bcb40e3b2aed337e54ccd32cbd79d18b1fdf
Denis Beliakov <ddenn@outlook.com>
2022-11-02 15:01:39 +0300

    Initial Commit

---
+++ Sprint03Task01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sprint03Task01
+//
+//  Created by Денис Беляков on 02.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Sprint03Task01UITests/Sprint03Task01UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Sprint03Task01UITestsLaunchTests.swift
+//  Sprint03Task01UITests
+//
+//  Created by Денис Беляков on 02.11.2022.
+//
+
+import XCTest
+
+final class Sprint03Task01UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
